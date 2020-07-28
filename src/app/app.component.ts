1b9841e7a95458f631d3c323b11aa0b5568d3865
unknown <meida1800016025@webmail.uad.ac.id>
2020-07-28 22:01:21 +0700

    ProjectAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { DetailDataComponent } from './detail-data/detail-data.component';
+import { DialogKonfirmasiComponent } from './dialog-konfirmasi/dialog-konfirmasi.component';
+import {ApiService} from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aplikasiCRUD';
-}
+  constructor(
+    public dialog:MatDialog,
+    public api:ApiService
+    )
+  {
+    this.getData(); //kode tambahan
+  }
+  dataSiswa:any=[];
+      getData()
+      {
+        this.api.baca().subscribe(res=>{
+      this.dataSiswa=res;
+      })
+    }
+    //fungsi untuk menampilkan dialog penambahan data baru
+    buat()
+  {
+    const dialogRef = this.dialog.open(TambahDataComponent, {
+      width: '450px', 
+      data:null     
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.getData(); // data diambil lagi dari server saat dialog tertutup
+    });
+  }
+  detail(item)
+      {
+        const dialogRef = this.dialog.open(DetailDataComponent, {
+          width: '450px',  
+          data:item
+        });	
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('The dialog was closed');     
+        });
+    }
+    konfirmasiHapus(id)
+    {
+       const dialogRef = this.dialog.open(DialogKonfirmasiComponent, {
+         width: '450px',      
+       });
+       dialogRef.afterClosed().subscribe(result => {
+         if(result == true)
+         {
+           console.log('Menghapus data');
+           this.api.hapus(id).subscribe(res=>{
+           
+           this.getData();
+           })
+         }
+       });
+    }
+    edit(data)
+    {
+      const dialogRef = this.dialog.open(TambahDataComponent, {
+        width: '450px',
+        data:data
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        this.getData();    
+      });
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,61 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import { LoginComponent } from './login/login.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { DetailDataComponent } from './detail-data/detail-data.component';
+import { DialogKonfirmasiComponent } from './dialog-konfirmasi/dialog-konfirmasi.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TambahDataComponent,
+    DetailDataComponent,
+    DialogKonfirmasiComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatDialogModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule 
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[
+    TambahDataComponent,
+    DetailDataComponent,
+    DialogKonfirmasiComponent
+  ]
+
 })
 export class AppModule { }
